7a4173f46331ec39ae14929fd5bc7314b20847a4
David Maurer <davemaurermail@gmail.com>
2015-09-01 11:30:25 -0600

    setup mailer with rails g mailer notifications_mailer. create class in notifications_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,5 +3,12 @@ class NotificationsController < ApplicationController
   end
 
   def create
+    NotificationsMailer.contact(email_params).deliver_now #name contact is arbitrary can be whatever you want
+  end
+
+  private
+
+  def email_params
+    params.permit(:name, :email, :message)
   end
 end
